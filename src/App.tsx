d41f3cf156a6f62a80e469f74f52ce2140b517a9
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2023-07-14 20:25:11 +0200

    Props with React.FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Todos items={["Todo 1", "Todo 2"]} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,11 @@
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
